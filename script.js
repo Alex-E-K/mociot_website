e96b88ea0b3720f2595cacf3439ec68f995eb679
Alexander Keller <a.e.keller@outlook.de>
2021-11-27 11:33:50 +0100

    Test navigator platform

---
+++ script.js
@@ -0,0 +1,19 @@
+var OSName = "Unknown OS";
+
+function getOSName() {
+    if (navigator.userAgentData.platform.indexOf("Win") != -1) {
+        OSName = "Windows";
+    } else if (navigator.userAgentData.platform.indexOf("Mac") != -1) {
+        OSName = "MacOS";
+    } else if (navigator.userAgentData.platform.indexOf("X11") != -1) {
+        OSName = "Unix";
+    } else if (navigator.userAgentData.platform.indexOf("Linux") != -1) {
+        OSName = "Linux";
+    } else if (navigator.userAgentData.platform.indexOf("iPhone") != -1 || navigator.userAgentData.platform.indexOf("iPad") != -1) {
+        OSName = "iOS";
+    } else if (navigator.userAgentData.platform.indexOf("Android") != -1) {
+        OSName = "Android";
+    }
+
+    document.getElementById("UA").innerText = "Hilfe";
+}
